65269ae5a9c0303e60084e3f9628ac97afc02f54
Greg Ziegan <grz5@case.edu>
2014-10-14 21:25:40 -0400

    include sample svm code for reference

--- src/ssvm.py
+++ src/ssvm.py
@@ -1,6 +1,7 @@
 from svm import SupportVectorMachine
 from utils import get_svm_inputs
 import numpy.linalg
+import math
 
 DATA_DIRECTORY = '../data/'
 
@@ -9,6 +10,11 @@ class SmoothSupportVectorMachine(SupportVectorMachine):
 
     def __init__(self):
         super(SupportVectorMachine, self).__init__()
+
+    def smoothing_function(self, x, alpha):
+        assert alpha > 0
+        return x + ((1/alpha) * math.log(1 + math.pow(math.e, (-1 * alpha * x))))
+
 
 if __name__ == '__main__':
     SmoothSupportVectorMachine.solve_svm(*get_svm_inputs())
